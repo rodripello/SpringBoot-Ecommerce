8bf5d8b155145ad269fc5ebcd6ac52db451fc843
cliente <rodrifp2016@gmail.com>
2023-01-17 14:02:34 -0300

    Se agrega model del proyecto y estructura inicial

---
+++ src/main/java/com/SpringBoot/Ecommerce/model/DetalleOrden.java
@@ -0,0 +1,73 @@
+package com.SpringBoot.Ecommerce.model;
+
+public class DetalleOrden {
+    private Integer id;
+    private String nombre;
+    private Double cantidad;
+    private Double precio;
+    private Double total;
+
+    public DetalleOrden() {
+
+    }
+
+    public DetalleOrden(Integer id, String nombre, Double cantidad, Double precio, Double total) {
+        super();
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.total = total;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "DetalleOrden{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", cantidad=" + cantidad +
+                ", precio=" + precio +
+                ", total=" + total +
+                '}';
+    }
+}
---
+++ src/main/java/com/SpringBoot/Ecommerce/model/Usuario.java
@@ -0,0 +1,106 @@
+package com.SpringBoot.Ecommerce.model;
+
+public class Usuario {
+    private Integer id;
+    private String name;
+    private String username;
+    private String email;
+    private String direccion;
+    private String telefono;
+    private String tipo;
+    private String password;
+
+    public Usuario() {
+
+    }
+
+    public Usuario(Integer id, String name, String username, String email, String direccion, String telefono, String tipo, String password) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.tipo = tipo;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
